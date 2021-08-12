6c57fc65dbfa8d6ba2f23cbafeb5cd977ba6aaa3
Ademir <ademir.m.junior@gmail.com>
2021-08-11 21:39:29 -0300

    Develop exchange data between Activities

--- app/src/main/java/br/edu/utfpr/activitycall/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/activitycall/MainActivity.java
@@ -2,13 +2,27 @@ package br.edu.utfpr.activitycall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextPhrase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextPhrase = findViewById(R.id.editTextPhrase);
+    }
+
+    public void send(View view) {
+        Intent intent = new Intent(this, ShowData.class);
+
+        intent.putExtra(ShowData.PHRASE, editTextPhrase.getText().toString());
+        startActivity(intent);
     }
 }
--- app/src/main/java/br/edu/utfpr/activitycall/ShowData.java
+++ app/src/main/java/br/edu/utfpr/activitycall/ShowData.java
@@ -2,13 +2,38 @@ package br.edu.utfpr.activitycall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ShowData extends AppCompatActivity {
+
+    public static final String PHRASE = "PHRASE";
+
+    private TextView textViewData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_data);
+
+        setTitle(getString(R.string.received_data));
+
+        textViewData = findViewById(R.id.textViewData);
+
+        Intent intent = getIntent();
+
+        Bundle bundle = intent.getExtras();
+
+        if (bundle != null) {
+            String phrase = bundle.getString(PHRASE);
+
+            textViewData.setText(phrase);
+        }
+    }
+
+    public void goBack(View view) {
+        finish();
     }
 }
